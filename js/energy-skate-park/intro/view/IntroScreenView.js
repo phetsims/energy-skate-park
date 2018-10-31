f853d251c0ed6d84d7853027837a1e89ceaa2f0d
Jesse <jesse.greenberg@colorado.edu>
2018-10-31 14:11:53 -0400

    temporary mockup image for intro screen

--- js/energy-skate-park/intro/view/IntroScreenView.js
+++ js/energy-skate-park/intro/view/IntroScreenView.js
@@ -12,8 +12,15 @@ define( function( require ) {
   var BackgroundNode = require( 'ENERGY_SKATE_PARK/energy-skate-park/view/BackgroundNode' );
   var energySkatePark = require( 'ENERGY_SKATE_PARK/energySkatePark' );
   var EnergySkateParkScreenView = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/EnergySkateParkScreenView' );
+  var HSlider = require( 'SUN/HSlider' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Image = require( 'SCENERY/nodes/Image' );
+  var Property = require( 'AXON/Property' );
   var MassComboBox = require( 'ENERGY_SKATE_PARK/energy-skate-park/intro/view/MassComboBox' );
+  var Range = require( 'DOT/Range' );
+
+  // images
+  var mockupImage = require( 'image!ENERGY_SKATE_PARK/mockup.png' );
 
   /**
    * @constructor
@@ -28,6 +35,17 @@ define( function( require ) {
     // add the combo box to the back layer so it is behind the skater
     this.addToBottomLayer( this.massComboBox );
     this.massComboBox.bottom = this.layoutBounds.height - BackgroundNode.earthHeight - 5;
+
+    // Show the mock-up and a slider to change its transparency
+    // TODO: temporary, just to do some pixel polishing
+    var mockupOpacityProperty = new Property( 0.2 );
+    var image = new Image( mockupImage, {
+      pickable: false,
+      scale: this.layoutBounds.width / mockupImage.width
+    } );
+    mockupOpacityProperty.linkAttribute( image, 'opacity' );
+    this.addChild( image );
+    this.addChild( new HSlider( mockupOpacityProperty, new Range( 0, 1 ), { top: 10, left: 500 } ) );
   }
 
   energySkatePark.register( 'IntroScreenView', IntroScreenView );
