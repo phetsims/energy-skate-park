05c87b45e400d3fc2d6f3f22a307d793dd2ee602
samreid <reids@colorado.edu>
2018-11-19 12:55:08 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/energy-skate-park/common/SplineEvaluation.js
+++ js/energy-skate-park/common/SplineEvaluation.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2018, University of Colorado Boulder
 
 /**
  * Use these modified variants of numeric.js spline code because they are much faster!
--- js/energy-skate-park/intro/IntroConstants.js
+++ js/energy-skate-park/intro/IntroConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2018, University of Colorado Boulder
 
 /**
  * Constants used in the intro screen.
