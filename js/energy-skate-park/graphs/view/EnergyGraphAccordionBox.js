7463e3f706fcce5ef9b220a863fda14c550f4def
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-06-12 20:36:06 -0400

    use XYTimePlot, see #42

--- js/energy-skate-park/graphs/view/EnergyGraphAccordionBox.js
+++ js/energy-skate-park/graphs/view/EnergyGraphAccordionBox.js
@@ -13,12 +13,13 @@ define( function( require ) {
   const AccordionBox = require( 'SUN/AccordionBox' );
   const energySkatePark = require( 'ENERGY_SKATE_PARK/energySkatePark' );
   const EraserButton = require( 'SCENERY_PHET/buttons/EraserButton' );
+  const Emitter = require( 'AXON/Emitter' );
   const Dimension2 = require( 'DOT/Dimension2' );
   const Text = require( 'SCENERY/nodes/Text' );
   const EnergySkateParkColorScheme = require( 'ENERGY_SKATE_PARK/energy-skate-park/view/EnergySkateParkColorScheme' );
   const Node = require( 'SCENERY/nodes/Node' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  const XYPlot = require( 'GRIDDLE/XYPlot' );
+  const XYTimePlot = require( 'GRIDDLE/XYTimePlot' );
   const XYDataSeries = require( 'GRIDDLE/XYDataSeries' );
   const VerticalCheckboxGroup = require( 'SUN/VerticalCheckboxGroup' );
 
@@ -31,13 +32,17 @@ define( function( require ) {
   const positionLabelString = require( 'string!ENERGY_SKATE_PARK/positionLabel' );
   const plotsEnergyGraphString = require( 'string!ENERGY_SKATE_PARK/plots.energy-graph' );
 
+  // constants
+  const GRAPH_WIDTH = 475;
+  const GRAPH_HEIGHT = 125;
+
   class EnergyGraphAccordionBox extends AccordionBox {
 
     /**
      * TODO: JSDOC
      */
     constructor( model ) {
-      
+
       // the parent for all content of the accordion box
       const contentNode = new Node();
 
@@ -85,6 +90,9 @@ define( function( require ) {
         buttonXMargin: 5,
         buttonYMargin: 5
       } );
+
+      // @public
+      this.energyPlot = energyPlot;
     }
 
     static createCheckboxItem( property, labelString, labelFill ) {
@@ -102,15 +110,21 @@ define( function( require ) {
    * XY Plot for a energy vs time. Includes labels and zoom buttons to change the zoom along the y axis (energy), and
    * an eraser button that clears the plot.
    */
-  class EnergyXYPlot extends XYPlot {
+  class EnergyXYPlot extends XYTimePlot {
 
     /**
      * @param {EnergySkateParkModel} model
      */
     constructor( model ) {
+
+      const dragEndedEmitter = new Emitter();
+      const dragStartedEmitter = new Emitter();
+
+      let pausedOnDragStart = true;
+
       super( {
-        width: 475,
-        height: 125,
+        width: GRAPH_WIDTH,
+        height: GRAPH_HEIGHT,
 
         maxX: 20,
         minY: -3000,
@@ -123,7 +137,28 @@ define( function( require ) {
 
         lineDash: [ 4, 4 ],
         showVerticalIntermediateLines: false,
-        showHorizontalIntermediateLines: false
+        showHorizontalIntermediateLines: false,
+
+        cursorOptions: {
+          startDrag: ( event, listener ) => {
+            pausedOnDragStart = model.pausedProperty.get();
+
+            if ( !pausedOnDragStart ) {
+              model.pausedProperty.set( true );
+            }
+
+            // workaround because `this` is not accessible yet
+            dragStartedEmitter.emit();
+          },
+          endDrag: ( event, listener ) => {
+            if ( !pausedOnDragStart ) {
+              model.pausedProperty.set( false );
+            }
+
+            // clear all data up to this point, we will begin recording from here
+            dragEndedEmitter.emit(); // a workaround since we don't have access to `this` yet
+          }
+        }
       } );
 
       // @private
@@ -133,10 +168,15 @@ define( function( require ) {
       this.totalEnergyDataSeries = new XYDataSeries( { color: EnergySkateParkColorScheme.totalEnergy } );
 
       model.skaterSamples.addItemAddedListener( skaterState => {
-        this.kineticEnergyDataSeries.addPoint( skaterState.time, skaterState.getKineticEnergy() );
-        this.potentialEnergyDataSeries.addPoint( skaterState.time, skaterState.getPotentialEnergy() );
-        this.thermalEnergyDataSeries.addPoint( skaterState.time, skaterState.thermalEnergy );
-        this.totalEnergyDataSeries.addPoint( skaterState.time, skaterState.getTotalEnergy() );
+
+        const time = skaterState.time;
+
+        this.kineticEnergyDataSeries.addPoint( time, skaterState.getKineticEnergy() );
+        this.potentialEnergyDataSeries.addPoint( time, skaterState.getPotentialEnergy() );
+        this.thermalEnergyDataSeries.addPoint( time, skaterState.thermalEnergy );
+        this.totalEnergyDataSeries.addPoint( time, skaterState.getTotalEnergy() );
+
+        this.setCursorTime( time );
       } );
 
       // series rendered in order, this order matches Java version
@@ -145,10 +185,45 @@ define( function( require ) {
       this.addSeries( this.kineticEnergyDataSeries, true );
       this.addSeries( this.totalEnergyDataSeries, true );
 
+      dragEndedEmitter.addListener( () => {
+        const timeOnEnd = this.getCursorTime();
+
+        model.runningTimeProperty.set( timeOnEnd );
+
+        // clear all data that has time great than the cursor time - this assumes that the time data is in order
+        for ( let i = 0; i < this.dataSeriesList.length; i++ ) {
+          const dataSeries = this.dataSeriesList[ i ];
+          const timeData = dataSeries.getXPoints();
+          for ( let j = 0; j < dataSeries.getLength(); j++ ) {
+            if ( timeData[ j ] >= timeOnEnd ) {
+              const startIndex = j;
+              const endIndex = dataSeries.getLength();
+
+              dataSeries.removePoints( startIndex, endIndex );
+              break;
+            }
+          }
+        }
+      } );
+
       model.kineticEnergyDataVisibleProperty.linkAttribute( this.seriesViewMap[ this.kineticEnergyDataSeries.uniqueId ], 'visible' );
       model.potentialEnergyDataVisibleProperty.linkAttribute( this.seriesViewMap[ this.potentialEnergyDataSeries.uniqueId ], 'visible' );
       model.thermalEnergyDataVisibleProperty.linkAttribute( this.seriesViewMap[ this.thermalEnergyDataSeries.uniqueId ], 'visible' );
       model.totalEnergyDataVisibleProperty.linkAttribute( this.seriesViewMap[ this.totalEnergyDataSeries.uniqueId ], 'visible' );
+    }
+
+    startDrag( event, listener ) {
+      this.pausedOnDragStart = this.model.pausedProperty.get();
+
+      if ( !this.pausedOnDragStart ) {
+        this.model.pausedProperty.set( false );
+      }
+    }
+
+    endDrag( event, listener ) {
+      if ( !this.pausedOnDragStart ) {
+        this.model.pausedProperty.set( false );
+      }
     }
 
     clearEnergyDataSeries() {
