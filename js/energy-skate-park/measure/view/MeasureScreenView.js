f4ce9277b8de640d2debdd16aa291b72ee43cccf
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-07-17 15:44:26 -0400

    convert to es6

--- js/energy-skate-park/measure/view/MeasureScreenView.js
+++ js/energy-skate-park/measure/view/MeasureScreenView.js
@@ -1,63 +1,60 @@
 // Copyright 2018-2019, University of Colorado Boulder
 
 /**
- * Energ
+ * The ScreenView for the "Measure" Screen.
+ * 
  * @author Jesse Greenberg
  */
-
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var energySkatePark = require( 'ENERGY_SKATE_PARK/energySkatePark' );
-  var EnergySkateParkScreenView = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/EnergySkateParkScreenView' );
-  var FrictionSlider = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/FrictionSlider' );
-  var GravityNumberControl = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/GravityNumberControl' );
-  var MassNumberControl = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/MassNumberControl' );
-  var SkaterSamplesNode = require( 'ENERGY_SKATE_PARK/energy-skate-park/measure/view/SkaterSamplesNode' );
-  var SkaterPathSensorNode = require( 'ENERGY_SKATE_PARK/energy-skate-park/measure/view/SkaterPathSensorNode' );
-  var inherit = require( 'PHET_CORE/inherit' );
+  const energySkatePark = require( 'ENERGY_SKATE_PARK/energySkatePark' );
+  const EnergySkateParkScreenView = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/EnergySkateParkScreenView' );
+  const FrictionSlider = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/FrictionSlider' );
+  const GravityNumberControl = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/GravityNumberControl' );
+  const MassNumberControl = require( 'ENERGY_SKATE_PARK/energy-skate-park/common/view/MassNumberControl' );
+  const SkaterSamplesNode = require( 'ENERGY_SKATE_PARK/energy-skate-park/measure/view/SkaterSamplesNode' );
+  const SkaterPathSensorNode = require( 'ENERGY_SKATE_PARK/energy-skate-park/measure/view/SkaterPathSensorNode' );
 
   /**
    * @constructor
    * @param {EnergySkateParkTrackSetModel} model
    * @param {Tandem} tandem
    */
-  function MeasureScreenView( model, tandem ) {
+  class MeasureScreenView extends EnergySkateParkScreenView {
 
-    var measureControls = [
-      new FrictionSlider( model.frictionProperty, tandem.createTandem( 'frictionSlider' ) ),
-      new GravityNumberControl( model.skater.gravityMagnitudeProperty, tandem.createTandem( 'gravitySlider' ), {
-        decimalPlaces: 1
-      } ),
-      new MassNumberControl( model.skater.massProperty, model.skater.massRange, tandem.createTandem( 'massNumberControl' ) )
-    ];
-    EnergySkateParkScreenView.call( this, model, measureControls, tandem, {
-      showBarGraph: false,
-      showSkaterPath: true,
-      visibilityControlsOptions: {
-        showSkaterPathCheckbox: true,
-        showReferenceHeightCheckbox: true,
-        showStickToTrackCheckbox: true,
-        showBarGraphCheckbox: false
-      }
-    } );
+    constructor( model, tandem ) {
+      const measureControls = [
+        new FrictionSlider( model.frictionProperty, tandem.createTandem( 'frictionSlider' ) ),
+        new GravityNumberControl( model.skater.gravityMagnitudeProperty, tandem.createTandem( 'gravitySlider' ), {
+          decimalPlaces: 1
+        } ),
+        new MassNumberControl( model.skater.massProperty, model.skater.massRange, tandem.createTandem( 'massNumberControl' ) )
+      ];
 
-    // @private - for layout
-    this.pathSensor = new SkaterPathSensorNode( model.skaterSamples, model.sensorPositionProperty, this.modelViewTransform, {
-      tandem: tandem.createTandem( 'pathSensor' )
-    } );
-    this.pathSensor.top = this.modelViewTransform.modelToViewDeltaY( -2 );
+      super( model, measureControls, tandem, {
+        showBarGraph: false,
+        showSkaterPath: true,
+        visibilityControlsOptions: {
+          showSkaterPathCheckbox: true,
+          showReferenceHeightCheckbox: true,
+          showStickToTrackCheckbox: true,
+          showBarGraphCheckbox: false
+        }
+      } );
 
-    this.addToTopLayer( new SkaterSamplesNode( model, this.modelViewTransform ) );
-    this.addToTopLayer( this.pathSensor );
-  }
+      // @private - for layout
+      this.pathSensor = new SkaterPathSensorNode( model.skaterSamples, model.sensorPositionProperty, this.modelViewTransform, {
+        tandem: tandem.createTandem( 'pathSensor' )
+      } );
+      this.pathSensor.top = this.modelViewTransform.modelToViewDeltaY( -2 );
 
-  energySkatePark.register( 'MeasureScreenView', MeasureScreenView );
+      this.addToTopLayer( new SkaterSamplesNode( model, this.modelViewTransform ) );
+      this.addToTopLayer( this.pathSensor );
+    }
 
-  return inherit( EnergySkateParkScreenView, MeasureScreenView, {
-
-    layout: function( width, height ) {
+    layout( width, height ) {
       EnergySkateParkScreenView.prototype.layout.call( this, width, height );
 
       // in the measure screen the legend is in the top left of the screen
@@ -65,5 +62,8 @@ define( function( require ) {
 
       this.pathSensor.leftTop = this.pieChartLegend.leftBottom.plusXY( 0, 10 );
     }
-  } );
+  }
+
+  return energySkatePark.register( 'MeasureScreenView', MeasureScreenView );
+
 } );
