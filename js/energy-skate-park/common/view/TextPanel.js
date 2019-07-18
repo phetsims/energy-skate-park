f0717c3c8135e37b0f861ff1143eaf591f655784
phet-dev <phetadmin@gmail.com>
2019-07-18 03:05:54 -0600

    update copyright dates

--- js/energy-skate-park/common/view/TextPanel.js
+++ js/energy-skate-park/common/view/TextPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * Much of the text in this sim is surrounded by a transparent white panel to assist with visibility. This type
