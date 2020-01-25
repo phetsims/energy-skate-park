8d5f3fc4fa20a4ae0f3367de67c42d6cb25a93b7
phet-dev <phetadmin@gmail.com>
2020-01-25 03:15:03 -0700

    update copyright dates from daily grunt work

--- js/energy-skate-park/common/model/SkaterSample.js
+++ js/energy-skate-park/common/model/SkaterSample.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * A collection of physical values and data about the Skater at a particular time. This data can be plotted visually
