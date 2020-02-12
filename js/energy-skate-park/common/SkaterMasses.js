2b3f11df348b7871da9f8379faf3cc09b216ff73
phet-dev <phetadmin@gmail.com>
2020-02-12 03:14:58 -0700

    update copyright dates from daily grunt work

--- js/energy-skate-park/common/SkaterMasses.js
+++ js/energy-skate-park/common/SkaterMasses.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * Constants related to the skater in Energy Skate Park.
--- js/energy-skate-park/common/view/SceneSelectionRadioButtonGroup.js
+++ js/energy-skate-park/common/view/SceneSelectionRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2019, University of Colorado Boulder
+// Copyright 2013-2020, University of Colorado Boulder
 
 /**
  * Radio button group for choosing one of the tracks, used in a "Track Set" screen of EnergySkatePark. This changes
