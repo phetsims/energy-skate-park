837f306ed78e7a5405f666ba186332d01a605643
samreid <reids@colorado.edu>
2020-02-28 09:35:15 -0700

    Move comment, see https://github.com/phetsims/energy-skate-park/issues/202

--- js/energy-skate-park-main.js
+++ js/energy-skate-park-main.js
@@ -7,7 +7,7 @@
  */
 
 import SimLauncher from '../../joist/js/SimLauncher.js';
-import '../../scenery/js/util/Color.js';
+import '../../scenery/js/util/Color.js'; // Fix a circular loading problem when using this in EnergySkateParkColorScheme
 import Tandem from '../../tandem/js/Tandem.js';
 import energySkateParkStrings from './energy-skate-park-strings.js';
 import EnergySkateParkSim from './energy-skate-park/common/EnergySkateParkSim.js';
@@ -15,8 +15,6 @@ import GraphsScreen from './energy-skate-park/graphs/GraphsScreen.js';
 import IntroScreen from './energy-skate-park/intro/IntroScreen.js';
 import LabScreen from './energy-skate-park/lab/LabScreen.js';
 import MeasureScreen from './energy-skate-park/measure/MeasureScreen.js';
-
-// Fix a circular loading problem when using this in EnergySkateParkColorScheme
 
 const energySkateParkTitleString = energySkateParkStrings[ 'energy-skate-park' ].title;
 
