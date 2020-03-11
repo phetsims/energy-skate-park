a64ae0257c3811a90b37a0f5f135fc025a4f5b84
Jesse Greenberg <jesse.greenberg@colorado.edu>
2020-03-11 17:27:55 -0400

    address review comment by replacing map wiith ternary operator, see #202

--- js/energy-skate-park/intro/view/IntroScreenView.js
+++ js/energy-skate-park/intro/view/IntroScreenView.js
@@ -11,15 +11,6 @@ import SkaterMasses from '../../common/SkaterMasses.js';
 import EnergySkateParkTrackSetScreenView from '../../common/view/EnergySkateParkTrackSetScreenView.js';
 import SkaterNode from '../../common/view/SkaterNode.js';
 
-// constants
-const MASS_IMAGE_MAP = new Map();
-MASS_IMAGE_MAP.set( SkaterMasses.SKATER_1_MASS, SkaterNode.SkaterImage.SKATER_1 );
-MASS_IMAGE_MAP.set( SkaterMasses.SKATER_2_MASS, SkaterNode.SkaterImage.SKATER_2 );
-MASS_IMAGE_MAP.set( SkaterMasses.SKATER_3_MASS, SkaterNode.SkaterImage.SKATER_3 );
-MASS_IMAGE_MAP.set( SkaterMasses.SKATER_4_MASS, SkaterNode.SkaterImage.SKATER_4 );
-MASS_IMAGE_MAP.set( SkaterMasses.SKATER_5_MASS, SkaterNode.SkaterImage.SKATER_5 );
-MASS_IMAGE_MAP.set( SkaterMasses.DOG_MASS, SkaterNode.SkaterImage.DOG );
-
 class IntroScreenView extends EnergySkateParkTrackSetScreenView {
 
   /**
@@ -43,10 +34,15 @@ class IntroScreenView extends EnergySkateParkTrackSetScreenView {
 
     // in the intro screen, each allowable mass has a unique skater image
     model.skater.massProperty.link( mass => {
-      assert && assert( MASS_IMAGE_MAP.has( mass ), 'no image supported for that mass' );
+      const newSkaterImage = mass === SkaterMasses.SKATER_1_MASS ? SkaterNode.SkaterImage.SKATER_1 :
+                       SkaterMasses.SKATER_2_MASS ? SkaterNode.SkaterImage.SKATER_2 :
+                       SkaterMasses.SKATER_3_MASS ? SkaterNode.SkaterImage.SKATER_3 :
+                       SkaterMasses.SKATER_4_MASS ? SkaterNode.SkaterImage.SKATER_4 :
+                       SkaterMasses.SKATER_5_MASS ? SkaterNode.SkaterImage.SKATER_5 :
+                       SkaterMasses.DOG_MASS ? SkaterNode.SkaterImage.DOG : null;
+      assert && assert( newSkaterImage !== null, 'no image supported for mass ' + mass );
 
-      // REVIEW: I recommend writing this using if/else or ternary expressions instead of Map.
-      this.skaterNode.skaterImageProperty.set( MASS_IMAGE_MAP.get( mass ) );
+      this.skaterNode.skaterImageProperty.set( newSkaterImage );
     } );
   }
 }
