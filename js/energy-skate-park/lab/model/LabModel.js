90faf1bdd514670e265cf3618ecc5a68830d9522
Jesse Greenberg <jesse.greenberg@colorado.edu>
2020-03-19 12:34:12 -0400

    remove initialTracksOffsetVector from model, see #238 and #239

--- js/energy-skate-park/lab/model/LabModel.js
+++ js/energy-skate-park/lab/model/LabModel.js
@@ -4,10 +4,11 @@
  * Model for the "Lab" screen in Energy Skate Park. This is an EnergySkateParkPlaygroundModel, with fully
  * interactive tracks.
  *
+ * TODO: This type can be deleted
+ *
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import Vector2 from '../../../../../dot/js/Vector2.js';
 import energySkatePark from '../../../energySkatePark.js';
 import EnergySkateParkPlaygroundModel from '../../common/model/EnergySkateParkPlaygroundModel.js';
 
@@ -17,11 +18,7 @@ class LabModel extends EnergySkateParkPlaygroundModel {
    * @param {Tandem} tandem
    */
   constructor( tandem ) {
-    super( tandem, {
-
-      // Draggable tracks in the lab screen start at a different position in the "Lab" screen
-      initialTracksOffsetVector: new Vector2( -2, -0.75 )
-    } );
+    super( tandem );
   }
 }
 
