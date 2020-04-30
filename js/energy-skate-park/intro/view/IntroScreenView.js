563dd326410f8bfe4686f9036767b29b4e69114d
Jesse <jesse.greenberg@colorado.edu>
2020-04-30 11:11:32 -0400

    replace ComboBox withy NumberControl in intro screen, see #263

--- js/energy-skate-park/intro/view/IntroScreenView.js
+++ js/energy-skate-park/intro/view/IntroScreenView.js
@@ -7,9 +7,7 @@
  */
 
 import energySkatePark from '../../../energySkatePark.js';
-import SkaterMasses from '../../common/SkaterMasses.js';
 import EnergySkateParkTrackSetScreenView from '../../common/view/EnergySkateParkTrackSetScreenView.js';
-import SkaterNode from '../../common/view/SkaterNode.js';
 
 class IntroScreenView extends EnergySkateParkTrackSetScreenView {
 
@@ -21,10 +19,6 @@ class IntroScreenView extends EnergySkateParkTrackSetScreenView {
     super( model, tandem.createTandem( 'introScreenView' ), {
       controlPanelOptions: {
         showMassControls: true,
-        massControlsOptions: {
-          includeMassNumberControl: false,
-          includeMassComboBox: true
-        },
         gravityControlsOptions: {
           includeGravityNumberControl: false,
           includeGravitySlider: true
@@ -33,19 +27,6 @@ class IntroScreenView extends EnergySkateParkTrackSetScreenView {
           showStickToTrackCheckbox: true
         }
       }
-    } );
-
-    // in the intro screen, each allowable mass has a unique skater image
-    model.skater.massProperty.link( mass => {
-      const newSkaterImage = mass === SkaterMasses.SKATER_1_MASS ? SkaterNode.SkaterImage.SKATER_1 :
-                             mass === SkaterMasses.SKATER_2_MASS ? SkaterNode.SkaterImage.SKATER_2 :
-                             mass === SkaterMasses.SKATER_3_MASS ? SkaterNode.SkaterImage.SKATER_3 :
-                             mass === SkaterMasses.SKATER_4_MASS ? SkaterNode.SkaterImage.SKATER_4 :
-                             mass === SkaterMasses.SKATER_5_MASS ? SkaterNode.SkaterImage.SKATER_5 :
-                             mass === SkaterMasses.DOG_MASS ? SkaterNode.SkaterImage.DOG : null;
-      assert && assert( newSkaterImage !== null, 'no image supported for mass ' + mass );
-
-      this.skaterNode.skaterImageProperty.set( newSkaterImage );
     } );
   }
 }
