021d4ce32f16daf4205b58ca2b82f4a9b705e820
Jesse <jesse.greenberg@colorado.edu>
2020-08-19 12:23:10 -0400

    add AR and DT to credits, see phetsims/energy-skate-park#287

--- js/energy-skate-park-main.js
+++ js/energy-skate-park-main.js
@@ -31,9 +31,9 @@ simLauncher.launch( () => {
 
   new EnergySkateParkSim( energySkateParkTitleString, screens, tandem, {
     credits: {
-      leadDesign: 'Ariel Paul, Noah Podolefsky, Sam Reid',
-      softwareDevelopment: 'Sam Reid',
-      team: 'Michael Dubson, Bryce Gruneich, Trish Loeblein, Emily B. Moore, Kathy Perkins',
+      leadDesign: 'Ariel Paul, Noah Podolefsky, Sam Reid, Amy Rouinfar',
+      softwareDevelopment: 'Sam Reid, Jesse Greenberg',
+      team: 'Michael Dubson, Bryce Gruneich, Trish Loeblein, Emily B. Moore, Kathy Perkins, Diana López Tavares',
       graphicArts: 'Sharon Siman-Tov, Amanda McGarry, Megan Lai',
       qualityAssurance: 'Steele Dalton, Oliver Orejola, Arnab Purkayastha, Bryan Yoelin'
     }
