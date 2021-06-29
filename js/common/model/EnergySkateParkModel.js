bded610c3a287a3d22c7ced4eb9a77e3a945d186
samreid <reids@colorado.edu>
2021-06-29 14:37:21 -0600

    Enable lint rule prefer-rest-params and convert arguments to ...args, see https://github.com/phetsims/chipper/issues/814

--- js/common/model/EnergySkateParkModel.js
+++ js/common/model/EnergySkateParkModel.js
@@ -65,11 +65,11 @@ const thrust = new Vector2( 0, 0 );
 const FRAME_RATE = 60;
 
 // Flag to enable debugging for physics issues
-const debug = EnergySkateParkQueryParameters.debugLog ? function() {
-  console.log.apply( console, arguments );
+const debug = EnergySkateParkQueryParameters.debugLog ? function( ...args ) {
+  console.log.apply( console, args );
 } : null;
-const debugAttachDetach = EnergySkateParkQueryParameters.debugAttachDetach ? function() {
-  console.log.apply( console, arguments );
+const debugAttachDetach = EnergySkateParkQueryParameters.debugAttachDetach ? function( ...args ) {
+  console.log.apply( console, args );
 } : null;
 
 // Track the model iterations to implement "slow motion" by stepping every Nth frame, see #210
