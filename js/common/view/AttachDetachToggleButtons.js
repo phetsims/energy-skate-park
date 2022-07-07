08793b508409b29665e41e41a954ea3d07f32e27
phet-dev <phetadmin@gmail.com>
2022-07-07 04:31:39 -0600

    update copyright dates from daily grunt work

--- js/common/view/AttachDetachToggleButtons.js
+++ js/common/view/AttachDetachToggleButtons.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Scenery node for the Attach/Detach toggle buttons which determine whether the skater can fly off the track or not.
--- js/common/view/SceneSelectionRadioButtonGroup.js
+++ js/common/view/SceneSelectionRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Radio button group for choosing one of the tracks, used in a "Track Set" screen of EnergySkatePark. This changes
