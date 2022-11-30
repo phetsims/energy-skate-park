c1ddc4895a70ee0c873f765a827f1b72cdb4a692
Jesse <jesse.greenberg@colorado.edu>
2022-11-30 18:38:00 -0500

    prevent resize for EnergyGraphAccordionBox, see #351

--- js/graphs/view/EnergyGraphAccordionBox.js
+++ js/graphs/view/EnergyGraphAccordionBox.js
@@ -170,6 +170,9 @@ class EnergyGraphAccordionBox extends AccordionBox {
       contentXMargin: 7,
       contentYSpacing: 0,
 
+      // do not resize the AccordionBox as EnergyChart labels animate (new labels may adjust bounds)
+      resize: false,
+
       // clicking doesn't expand since the title contains other controls
       titleBarExpandCollapse: false,
 
