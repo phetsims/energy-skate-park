e8a9893e86081a26b667ef4c6aa537f51f3e9542
phet-dev <phetadmin@gmail.com>
2023-01-11 04:14:00 -0700

    update copyright dates from daily grunt work

--- js/common/view/SkaterImages.js
+++ js/common/view/SkaterImages.js
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * Responsible for all images for the Skater in this sim. Collects the images into usable classes to support
