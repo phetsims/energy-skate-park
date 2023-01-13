53a68680e4efc3b180dcb2986d51abddcf521826
phet-dev <phetadmin@gmail.com>
2023-01-13 04:06:22 -0700

    update copyright dates from daily grunt work

--- js/common/view/EnergySkateParkPreferencesNode.js
+++ js/common/view/EnergySkateParkPreferencesNode.js
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * The node that is shown in the Preferences dialog of Energy Skate Park to control simulation
