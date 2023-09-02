3a35b5e352c6a9e19cbaf127fa35246b2314a132
phet-dev <phetadmin@gmail.com>
2023-09-02 03:13:42 -0600

    update copyright dates from daily grunt work

--- js/common/view/EnergySkateParkCheckboxItem.js
+++ js/common/view/EnergySkateParkCheckboxItem.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * A Checkbox in EnergySkatePark, which includes a label and an icon. Icons are generally aligned with other checkboxes
