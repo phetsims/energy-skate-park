d6822da18147ce5159c1e40b0cc2602aaa6954e4
phet-dev <phetadmin@gmail.com>
2023-11-30 03:14:02 -0700

    update copyright dates from daily grunt work

--- js/common/view/EnergySkateParkMassControls.js
+++ js/common/view/EnergySkateParkMassControls.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * Collection of controls for EnergySkateParkControlPanel that pertain to Skater mass.
