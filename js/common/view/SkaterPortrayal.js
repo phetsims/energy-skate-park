21a480200a3457ae8204d77f9ffa44d3d1462f58
phet-dev <phetadmin@gmail.com>
2024-03-12 03:15:17 -0600

    update copyright dates from daily grunt work

--- js/common/view/SkaterPortrayal.js
+++ js/common/view/SkaterPortrayal.js
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * A collection of SkaterImageSets. One SkaterPortrayal is active at a time. The user can select one of the
