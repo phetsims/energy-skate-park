823de032a890fb4f3d2042cd9330a6809ec352d7
pixelzoom <cmalley@pixelzoom.com>
2024-03-25 20:18:15 -0600

    simplify transform for Intro screen icon, https://github.com/phetsims/energy-skate-park/issues/382

--- js/intro/view/IntroScreenIcon.js
+++ js/intro/view/IntroScreenIcon.js
@@ -23,10 +23,9 @@ class IntroScreenIcon extends ScreenIcon {
 
     // Transform skaterImage to the desired position.
     skaterImage.localBoundsProperty.link( () => {
-      const transformMatrix = Matrix3.translation( 142, 120 );
+      const transformMatrix = Matrix3.translation( 187, 60 );
       transformMatrix.multiplyMatrix( Matrix3.rotation2( 1.1 * Math.PI / 3 ) );
       transformMatrix.multiplyMatrix( Matrix3.scaling( 0.5 ) );
-      transformMatrix.multiplyMatrix( Matrix3.translation( -skaterImage.width / 2, -skaterImage.height ) );
       skaterImage.setMatrix( transformMatrix );
     } );
 
