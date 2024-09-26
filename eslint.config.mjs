63611a3ca0f11e25dbf3a9706adcea4100029773
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:49 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for energy-skate-park.
+ * ESLint configuration for energy-skate-park.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
