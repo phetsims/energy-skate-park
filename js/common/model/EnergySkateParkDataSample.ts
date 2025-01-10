ad1bf2a9cffd86896c2a8f78ca279e5d98aaa8f4
Sam Reid <reids@colorado.edu>
2025-01-10 15:17:29 -0700

    Convert to TypeScript, see https://github.com/phetsims/energy-skate-park/issues/387

--- js/common/model/EnergySkateParkDataSample.ts
+++ js/common/model/EnergySkateParkDataSample.ts
@@ -16,34 +16,83 @@ import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Utils from '../../../../dot/js/Utils.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import energySkatePark from '../../energySkatePark.js';
+import SkaterState from './SkaterState.js';
+import Track from './Track.js';
 
 // constants
 // samples will fade out to this opacity before being fully removed from the model
 const MIN_OPACITY = 0.05;
 
-class EnergySkateParkDataSample {
+export default class EnergySkateParkDataSample {
+
+  // Values copied to avoid re-calculating when inspected
+  public readonly speed: number;
+  public referenceHeight: number;
+  public readonly position: Vector2;
+  public readonly time: number;
+  public readonly stickingToTrack: boolean;
+
+  // Skater's track at time of save
+  public readonly track: Track;
+
+  // Positions of the control points for the saved track, if there is one
+  private readonly trackControlPointPositions: Vector2[];
+
+  // Values copied from SkaterState, but these may change with the reference height
+  public kineticEnergy: number;
+  public potentialEnergy: number;
+  public thermalEnergy: number;
+  public totalEnergy: number;
+
+  // The skater's state at the time of sampling
+  public readonly skaterState: SkaterState;
+
+  // Friction value to be restored when scrubbing through data
+  public readonly friction: number;
+
+  // Time since this sample was added to the model, in seconds
+  public timeSinceAdded: number;
+
+  // Whether this sample is being inspected by the probe
+  public readonly inspectedProperty: BooleanProperty;
+
+  // The opacity of this skater sample, tied to visual representation
+  public readonly opacityProperty: NumberProperty;
+
+  // Emits an event when this EnergySkateParkDataSample has updated
+  public readonly updatedEmitter: Emitter;
+
+  // Samples which have initiated removal retain this percentage of opacity every animation frame
+  private readonly fadeDecay: number;
+
+  // Indicates that this sample should begin removal and will fade out
+  private _initiateRemove: boolean;
 
   /**
-   * @param {SkaterState} skaterState
-   * @param {number} friction - to be restored when scrubbing through data
-   * @param {number} time - in seconds
-   * @param {boolean} stickingToTrack
-   * @param {number} fadeDecay - samples being removed retain this percent of opacity (opacity = opacity * fadeDecay)
+   * @param skaterState - The skater's state at the time of sampling
+   * @param friction - to be restored when scrubbing through data
+   * @param time - in seconds
+   * @param stickingToTrack - whether the skater is sticking to the track
+   * @param fadeDecay - samples being removed retain this percent of opacity (opacity = opacity * fadeDecay)
    */
-  constructor( skaterState, friction, time, stickingToTrack, fadeDecay ) {
+  public constructor(
+    skaterState: SkaterState,
+    friction: number,
+    time: number,
+    stickingToTrack: boolean,
+    fadeDecay: number
+  ) {
+
     assert && assert( fadeDecay < 1, 'samples which have initiated removal need to fade away' );
 
-    // @public (read-only) - values copied to avoid re-calculating when inspected
     this.speed = skaterState.getSpeed();
     this.referenceHeight = skaterState.referenceHeight;
     this.position = new Vector2( skaterState.positionX, skaterState.positionY );
     this.time = time;
     this.stickingToTrack = stickingToTrack;
 
-    // @public (read-only) {Track} - Skater's track at time of save
     this.track = skaterState.track;
 
-    // @private {Vector2[]} - positions of the control points for the saved track, if there is one
     this.trackControlPointPositions = [];
     if ( this.track ) {
       this.track.controlPoints.forEach( controlPoint => {
@@ -51,48 +100,26 @@ class EnergySkateParkDataSample {
       } );
     }
 
-    // @public (read-only) {number} - values copied from SkaterState, but these may change with the reference height
-    // and become out of sync with SkaterState
     this.kineticEnergy = skaterState.getKineticEnergy();
     this.potentialEnergy = skaterState.getPotentialEnergy();
     this.thermalEnergy = skaterState.thermalEnergy;
     this.totalEnergy = skaterState.getTotalEnergy();
 
-    // @public (read-only)
     this.skaterState = skaterState;
-
-    // @public (read-only) {number}
     this.friction = friction;
-
-    // @public - in seconds, time since this sample was added to the model
     this.timeSinceAdded = 0;
-
-    // @public - whether or not this sample is being inspected by the probe
     this.inspectedProperty = new BooleanProperty( false );
-
-    // @public - the opacity of this skater sample, tied to visual representation
     this.opacityProperty = new NumberProperty( 1 );
-
-    // @public - emits an event when this EnergySkateParkDataSample has updated in some way, like when energies change
-    // due to a change in reference height
     this.updatedEmitter = new Emitter();
-
-    // @private {number} - EnergySkateParkDataSamples which have initiated removal will retain this percentage of opacity
-    // every animation frame. opacity = opacity * fadeDecay
     this.fadeDecay = fadeDecay;
-
-    // @private {boolean} - indicates that this sample should begin removal, and will fade out
     this._initiateRemove = false;
   }
 
   /**
    * Calculate new energies for this EnergySkateParkDataSample with the new reference height. Potential energy will be recalculated
    * and total energy will be adjusted accordingly to conserve energy. Thermal and kinetic energies should not change.
-   * @public
-   *
-   * @param {number} referenceHeight
    */
-  setNewReferenceHeight( referenceHeight ) {
+  public setNewReferenceHeight( referenceHeight: number ): void {
 
     this.referenceHeight = referenceHeight;
 
@@ -113,20 +140,16 @@ class EnergySkateParkDataSample {
   /**
    * Get the potential energy a particular reference height, using the other physical properties of the SkaterState
    * unchanged.
-   * @public
-   *
-   * @param {number} referenceHeight
-   * @returns {number}
    */
-  getPotentialEnergyAtReferenceHeight( referenceHeight ) {
+  public getPotentialEnergyAtReferenceHeight( referenceHeight: number ): number {
     return -this.skaterState.mass * this.skaterState.gravity * ( this.skaterState.positionY - referenceHeight );
   }
 
   /**
-   * @public
-   * @param {number} dt - in seconds
+   * Update the sample's state over time
+   * @param dt - in seconds
    */
-  step( dt ) {
+  public step( dt: number ): void {
     if ( this._initiateRemove ) {
       this.opacityProperty.set( this.opacityProperty.get() * this.fadeDecay );
     }
@@ -137,16 +160,12 @@ class EnergySkateParkDataSample {
   /**
    * Indicate that this skater sample is about to be removed. Opacity immediately is reduced, and after a short time
    * this sample will be completely removed.
-   * @public
    */
-  initiateRemove() {
+  public initiateRemove(): void {
     this._initiateRemove = true;
   }
+
+  public static readonly MIN_OPACITY = MIN_OPACITY;
 }
 
-// @public
-// @static
-EnergySkateParkDataSample.MIN_OPACITY = MIN_OPACITY;
-
 energySkatePark.register( 'EnergySkateParkDataSample', EnergySkateParkDataSample );
-export default EnergySkateParkDataSample;
