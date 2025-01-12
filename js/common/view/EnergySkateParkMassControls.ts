a478d171974d991477cdc6243f1b545c378735f5
Sam Reid <reids@colorado.edu>
2025-01-12 15:27:29 -0700

    Convert to TypeScript, see https://github.com/phetsims/energy-skate-park/issues/387

--- js/common/view/EnergySkateParkMassControls.ts
+++ js/common/view/EnergySkateParkMassControls.ts
@@ -6,27 +6,36 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import Emitter from '../../../../axon/js/Emitter.js';
+import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import Range from '../../../../dot/js/Range.js';
 import merge from '../../../../phet-core/js/merge.js';
+import IntentionalAny from '../../../../phet-core/js/types/IntentionalAny.js';
 import { VBox } from '../../../../scenery/js/imports.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
 import energySkatePark from '../../energySkatePark.js';
 import MassComboBox from './MassComboBox.js';
 import MassNumberControl from './MassNumberControl.js';
 import MassSlider from './MassSlider.js';
 
 class EnergySkateParkMassControls extends VBox {
+  private readonly massComboBox: MassComboBox | null;
 
   /**
-   * @param {NumberProperty} massProperty
-   * @param {BooleanProperty} userControlledProperty
-   * @param {Range} massRange - allowable range for the Skater mass
-   * @param {Property} skaterImageSetProperty - controls the current image for the SkaterNode
-   * @param {[type]} resetEmitter - broadcasts when EnergySkateParkModel has been reset
-   * @param {Node} listParent - parent Node for the ComboBox, if one is included
-   * @param {Tandem} tandem
-   * @param {Object} [options]
+   * @param massProperty
+   * @param userControlledProperty
+   * @param massRange - allowable range for the Skater mass
+   * @param skaterImageSetProperty - controls the current image for the SkaterNode
+   * @param resetEmitter - broadcasts when EnergySkateParkModel has been reset
+   * @param listParent - parent Node for the ComboBox, if one is included
+   * @param tandem
+   * @param [options]
    */
-  constructor( massProperty, userControlledProperty, massRange, skaterImageSetProperty, resetEmitter, listParent, tandem, options ) {
+  public constructor( massProperty: NumberProperty, userControlledProperty: BooleanProperty, massRange: Range, skaterImageSetProperty: Property<IntentionalAny>, resetEmitter: Emitter, listParent: Node, tandem: Tandem, options?: IntentionalAny ) {
 
+    // eslint-disable-next-line phet/bad-typescript-text
     options = merge( {
 
       // {boolean} whether or not a MassNumberControl is included in this set of controls, only a MassNumberControl
@@ -51,19 +60,21 @@ class EnergySkateParkMassControls extends VBox {
 
     let massComboBox = null;
     if ( options.includeMassComboBox ) {
-      massComboBox = new MassComboBox( massProperty, userControlledProperty, resetEmitter, listParent, tandem.createTandem( 'massComboBox' ), options.massComboBoxOptions );
+      massComboBox = new MassComboBox( massProperty, userControlledProperty, resetEmitter, listParent, tandem.createTandem( 'massComboBox' ) );
       children.push( massComboBox );
     }
 
     let massNumberControl = null;
     if ( options.includeMassNumberControl ) {
-      massNumberControl = new MassNumberControl( massProperty, userControlledProperty, massRange, tandem.createTandem( 'massNumberControl' ), options.massNumberControlOptions );
+      // @ts-expect-error
+      massNumberControl = new MassNumberControl( massProperty, userControlledProperty, massRange, tandem.createTandem( 'massNumberControl' ) );
       children.push( massNumberControl );
     }
 
     let massSlider = null;
     if ( options.includeMassSlider ) {
 
+      // @ts-expect-error
       massSlider = new MassSlider( massProperty, userControlledProperty, massRange, tandem.createTandem( 'massNumberControl' ) );
       children.push( massSlider );
     }
@@ -75,11 +86,8 @@ class EnergySkateParkMassControls extends VBox {
 
   /**
    * Set the width of the component to match the width of other controls in a containing control panel.
-   * @public
-   *
-   * @param width
    */
-  matchLayout( width ) {
+  public matchLayout( width: number ): void {
     if ( this.massComboBox ) {
       this.massComboBox.matchLayout( width );
     }
