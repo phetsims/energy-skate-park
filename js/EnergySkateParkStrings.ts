8c6e899e7747997c422606acf1beac6dbd7e7c42
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-01-27 12:39:32 -0700

    add "supportsDynamicLocale": true to sims that support it but didn't have it, https://github.com/phetsims/joist/issues/1005

--- js/EnergySkateParkStrings.ts
+++ js/EnergySkateParkStrings.ts
@@ -13,143 +13,91 @@ import energySkatePark from './energySkatePark.js';
 
 type StringsType = {
   'energy-skate-park': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
   'physicalControls': {
     'gravityControls': {
-      'gravity': string;
       'gravityStringProperty': LocalizedStringProperty;
-      'earth': string;
       'earthStringProperty': LocalizedStringProperty;
-      'jupiter': string;
       'jupiterStringProperty': LocalizedStringProperty;
-      'moon': string;
       'moonStringProperty': LocalizedStringProperty;
-      'gravityMetersPerSecondSquaredPattern': string;
       'gravityMetersPerSecondSquaredPatternStringProperty': LocalizedStringProperty;
-      'gravityNewtonsPerKilogramPattern': string;
       'gravityNewtonsPerKilogramPatternStringProperty': LocalizedStringProperty;
     };
     'massControls': {
-      'mass': string;
       'massStringProperty': LocalizedStringProperty;
-      'massKilogramsPattern': string;
       'massKilogramsPatternStringProperty': LocalizedStringProperty;
     };
-    'friction': string;
     'frictionStringProperty': LocalizedStringProperty;
-    'custom': string;
     'customStringProperty': LocalizedStringProperty;
-    'small': string;
     'smallStringProperty': LocalizedStringProperty;
-    'large': string;
     'largeStringProperty': LocalizedStringProperty;
-    'lots': string;
     'lotsStringProperty': LocalizedStringProperty;
-    'none': string;
     'noneStringProperty': LocalizedStringProperty;
-    'tiny': string;
     'tinyStringProperty': LocalizedStringProperty;
   };
   'energies': {
-    'thermal': string;
     'thermalStringProperty': LocalizedStringProperty;
-    'total': string;
     'totalStringProperty': LocalizedStringProperty;
-    'kinetic': string;
     'kineticStringProperty': LocalizedStringProperty;
-    'potential': string;
     'potentialStringProperty': LocalizedStringProperty;
-    'energy': string;
     'energyStringProperty': LocalizedStringProperty;
   };
   'plots': {
     'barGraph': {
-      'label': string;
       'labelStringProperty': LocalizedStringProperty;
     };
     'energyGraph': {
-      'label': string;
       'labelStringProperty': LocalizedStringProperty;
     };
     'pieChart': {
-      'label': string;
       'labelStringProperty': LocalizedStringProperty;
     };
-    'timeSwitchLabel': string;
     'timeSwitchLabelStringProperty': LocalizedStringProperty;
-    'positionSwitchLabel': string;
     'positionSwitchLabelStringProperty': LocalizedStringProperty;
-    'timeLabel': string;
     'timeLabelStringProperty': LocalizedStringProperty;
-    'positionLabel': string;
     'positionLabelStringProperty': LocalizedStringProperty;
-    'energyLabel': string;
     'energyLabelStringProperty': LocalizedStringProperty;
   };
   'visibilityControls': {
-    'grid': string;
     'gridStringProperty': LocalizedStringProperty;
-    'referenceHeight': string;
     'referenceHeightStringProperty': LocalizedStringProperty;
-    'path': string;
     'pathStringProperty': LocalizedStringProperty;
-    'speed': string;
     'speedStringProperty': LocalizedStringProperty;
   };
   'skaterControls': {
-    'label': string;
     'labelStringProperty': LocalizedStringProperty;
-    'skater1MassPattern': string;
     'skater1MassPatternStringProperty': LocalizedStringProperty;
-    'restartSkater': string;
     'restartSkaterStringProperty': LocalizedStringProperty;
   };
   'trackControls': {
-    'stickToTrack': string;
     'stickToTrackStringProperty': LocalizedStringProperty;
   };
   'screens': {
-    'intro': string;
     'introStringProperty': LocalizedStringProperty;
-    'graphs': string;
     'graphsStringProperty': LocalizedStringProperty;
-    'playground': string;
     'playgroundStringProperty': LocalizedStringProperty;
-    'measure': string;
     'measureStringProperty': LocalizedStringProperty;
   };
   'heightLabels': {
-    'zeroM': string;
     'zeroMStringProperty': LocalizedStringProperty;
-    'heightEqualsZero': string;
     'heightEqualsZeroStringProperty': LocalizedStringProperty;
   };
   'pathSensor': {
-    'energyJoulesPattern': string;
     'energyJoulesPatternStringProperty': LocalizedStringProperty;
-    'heightMetersPattern': string;
     'heightMetersPatternStringProperty': LocalizedStringProperty;
-    'speedMetersPerSecondPattern': string;
     'speedMetersPerSecondPatternStringProperty': LocalizedStringProperty;
   };
   'speedometer': {
-    'label': string;
     'labelStringProperty': LocalizedStringProperty;
-    'metersPerSecondPattern': string;
     'metersPerSecondPatternStringProperty': LocalizedStringProperty;
   };
   'measuringTape': {
-    'units': string;
     'unitsStringProperty': LocalizedStringProperty;
   };
   'preferences': {
-    'accelerationUnits': string;
     'accelerationUnitsStringProperty': LocalizedStringProperty;
-    'metersPerSecondSquared': string;
     'metersPerSecondSquaredStringProperty': LocalizedStringProperty;
-    'newtonsPerKilogram': string;
     'newtonsPerKilogramStringProperty': LocalizedStringProperty;
   }
 };
