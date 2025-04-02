2713d68b2b9098f2ca48da1fc79c95cee8c0a8bc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-04-02 10:44:00 -0600

    IOType requires parameter types, use AnyIOType for previous "any" default, https://github.com/phetsims/tandem/issues/261

--- js/common/model/ControlPoint.ts
+++ js/common/model/ControlPoint.ts
@@ -16,6 +16,7 @@ import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Vector2Property from '../../../../dot/js/Vector2Property.js';
 import optionize from '../../../../phet-core/js/optionize.js';
+import IntentionalAny from '../../../../phet-core/js/types/IntentionalAny.js';
 import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
 import PhetioObject, { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
@@ -109,7 +110,7 @@ export default class ControlPoint extends PhetioObject {
     return model.controlPointGroup.createNextElement( this.positionProperty.value.x, this.positionProperty.value.y, {} );
   }
 
-  public static readonly ControlPointIO = new IOType( 'ControlPointIO', {
+  public static readonly ControlPointIO = new IOType<IntentionalAny, IntentionalAny>( 'ControlPointIO', {
     valueType: ControlPoint,
     documentation: 'A control point that can manipulate the track.',
     toStateObject: controlPoint => {
