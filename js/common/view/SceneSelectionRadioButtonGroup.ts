101507ddc84fb2883ca959aa3abcfa9fea9cec15
Sam Reid <reids@colorado.edu>
2026-02-16 15:57:43 -0700

    Fix track name, see https://github.com/phetsims/energy-skate-park/issues/429

--- js/common/view/SceneSelectionRadioButtonGroup.ts
+++ js/common/view/SceneSelectionRadioButtonGroup.ts
@@ -85,7 +85,7 @@ export default class SceneSelectionRadioButtonGroup extends RectangularRadioButt
         const parabolaControlPoints = PremadeTracks.createParabolaControlPoints( model, Tandem.OPT_OUT, controlPointOptions );
         track = EnergySkateParkTrackSetModel.createPremadeTrack( model, parabolaControlPoints );
       }
-      else if ( trackType === 'SLOPE' ) {
+      else if ( trackType === 'RAMP' ) {
         const rampControlPoints = PremadeTracks.createSlopeControlPoints( model, Tandem.OPT_OUT, controlPointOptions );
         track = EnergySkateParkTrackSetModel.createPremadeTrack( model, rampControlPoints );
       }
