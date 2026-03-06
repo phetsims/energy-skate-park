445ed64fc739a498e3dbf923573d503bb682a7b3
Sam Reid <reids@colorado.edu>
2026-03-06 16:58:15 -0700

    Round friction slider aria text to nearest 0.001, see https://github.com/phetsims/energy-skate-park/issues/472

--- js/common/view/FrictionSlider.ts
+++ js/common/view/FrictionSlider.ts
@@ -10,6 +10,7 @@ import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Range from '../../../../dot/js/Range.js';
 import { roundToInterval } from '../../../../dot/js/util/roundToInterval.js';
+import { toFixed } from '../../../../dot/js/util/toFixed.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import energySkatePark from '../../energySkatePark.js';
 import EnergySkateParkFluent from '../../EnergySkateParkFluent.js';
@@ -30,7 +31,8 @@ export default class FrictionSlider extends PhysicalSlider {
     const range = new Range( EnergySkateParkConstants.MIN_FRICTION, EnergySkateParkConstants.MAX_FRICTION );
     super( EnergySkateParkFluent.physicalControls.frictionStringProperty, property, range, userControlledProperty, tandem, {
       sliderOptions: {
-        constrainValue: value => roundToInterval( value, FRICTION_INTERVAL )
+        constrainValue: value => roundToInterval( value, FRICTION_INTERVAL ),
+        createAriaValueText: ( value: number ) => toFixed( value, 3 )
       },
       valueChangeSoundGeneratorOptions: {
         numberOfMiddleThresholds: ( range.getLength() / FRICTION_INTERVAL ) - 1
