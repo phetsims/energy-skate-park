881bed3e6e071b0c7f86f04543ed155f568ac3f3
Sam Reid <reids@colorado.edu>
2026-03-12 15:49:09 -0600

    Set phetioFeatured: true for visibilityControlsPanel.visibleProperty, see https://github.com/phetsims/energy-skate-park/issues/507

--- js/common/view/VisibilityControlsPanel.ts
+++ js/common/view/VisibilityControlsPanel.ts
@@ -7,7 +7,7 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import { EmptySelfOptions, optionize4 } from '../../../../phet-core/js/optionize.js';
 import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import energySkatePark from '../../energySkatePark.js';
@@ -15,16 +15,30 @@ import EnergySkateParkConstants from '../EnergySkateParkConstants.js';
 import EnergySkateParkModel from '../model/EnergySkateParkModel.js';
 import EnergySkateParkVisibilityControls from './EnergySkateParkVisibilityControls.js';
 
+type SelfOptions = EmptySelfOptions;
+export type VisibilityControlsPanelOptions = SelfOptions & PanelOptions;
+
 export default class VisibilityControlsPanel extends Panel {
 
-  public constructor( model: EnergySkateParkModel, tandem: Tandem, providedOptions?: PanelOptions ) {
-    const options = combineOptions<PanelOptions>( { visiblePropertyOptions: { phetioFeatured: true } }, EnergySkateParkConstants.PANEL_OPTIONS, providedOptions );
+  public constructor( model: EnergySkateParkModel, tandem: Tandem, providedOptions?: VisibilityControlsPanelOptions ) {
+    const options = optionize4<VisibilityControlsPanelOptions, SelfOptions, PanelOptions>()(
+      {},
+      EnergySkateParkConstants.PANEL_OPTIONS,
+      {
+        tandem: tandem,
+        visiblePropertyOptions: {
+          phetioFeatured: true
+        }
+      },
+      providedOptions
+    );
 
-    const content = new EnergySkateParkVisibilityControls( model, tandem, {
+    const content = new EnergySkateParkVisibilityControls( model, tandem.createTandem( 'checkboxGroup' ), {
       showPieChartCheckbox: false,
       showSpeedCheckbox: false,
       showGridCheckbox: true,
-      showReferenceHeightCheckbox: true
+      showReferenceHeightCheckbox: true,
+      phetioVisiblePropertyInstrumented: false
     } );
 
     super( content, options );
